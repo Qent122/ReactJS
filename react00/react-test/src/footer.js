10c8099db654da189ec46faedd403a8a877dff97
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-07 16:37:40 +0200

    modification de l'header

--- react00/react-test/src/footer.js
+++ react00/react-test/src/footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="footer">
     <footer>
-      <div>&#169; C&C Compagnie</div>
+      <p>&#169; C&C Compagnie</p>
     </footer>
     </div>
   );
--- react00/react-test/src/header.js
+++ react00/react-test/src/header.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './header.css';
+import logo from './logo.svg'
 
 function header(){
     return(
         <div className='header'>
-            <h1>Bienvenue sur mon Cour sur React</h1>
+            <div className='presentation'>
+            <img src={logo} className='logo' />
+            <h1>Bienvenue sur mon cour sur React !</h1>
+            <img src={logo} className='logo' />
+            </div>
+            
+            <nav className='nav'>
+                <ul>
+                <li><a>link 1</a></li>
+                <li><a>link 2</a></li>
+                <li><a>link 3</a></li>
+                <li><a>link 4</a></li>
+
+                </ul>
+                
+            </nav>
         </div>
     )
 }
