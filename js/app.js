153e8bbfd9dd17b524e2c78feac54374662de114
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-07 11:28:18 +0200

    ajout html css js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// hello = function(){
+//     return "Hello World !"
+// }
+
+hello = () => {
+    return "Hello World !"
+
+}
+
+hello = () => "hello World !"
+
+// document.getElementById("app").innerHTML = hello()
+
+console.log(hello);
+
+// map()
+
+const tab=[2,"",3,4,5]
+
+const tab2 = tab.map(elementTab => {
+    return elementTab * 2
+})
+
+console.log(tab);
+console.log(tab2);
