ade0920cd1996291ae555664bc98eb4038d7bccc
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-10 14:46:53 +0200

    ajout axios

--- react00/react-test/src/app/App.js
+++ react00/react-test/src/app/App.js
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from "react";
 import logo from "../logo.svg";
 import "./App.css";
 
 function App() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
   return (
     <div className="App">
       <main className="App-main">
+        <div className="compteur">
+          Total : {count}
+          <button onClick={() => setCount(initialCount)}>Reset</button>
+          <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+            -
+          </button>
+          <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+            +
+          </button>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
