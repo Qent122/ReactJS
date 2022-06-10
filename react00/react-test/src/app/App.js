cc9d8f9a97315fc1da2414c73963045610fe2817
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-10 16:42:22 +0200

    ajout modal

--- react00/react-test/src/app/App.js
+++ react00/react-test/src/app/App.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react";
 import logo from "../logo.svg";
 import "./App.css";
+import Modal from "../modal/modal";
 
 function App() {
   const initialCount = 0;
+
   const [count, setCount] = useState(initialCount);
+  const [show, setShow] = useState(false);
+
+  function handleModal() {
+    setShow(!show);
+  }
 
   return (
     <div className="App">
       <main className="App-main">
+        {show && <Modal />}
+        <button onClick={handleModal}>
+          {show ? "Cacher" : "montrer"} La modal
+        </button>
+
         <div className="compteur">
           Total : {count}
           <button onClick={() => setCount(initialCount)}>Reset</button>
